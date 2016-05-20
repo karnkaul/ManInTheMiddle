0baca50f21d988a7cbfa23b1e68150d27ae16b73
Karn <karnkaul@gmail.com>
2016-05-19 18:37:05 -0700

    Separate Loader and PageManager Create PM prefab with both components Replace all scenes with prefab (and values)

---
+++ Scripts/Loader.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+using System.Collections;
+using Definitions;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Use this to branch to a different Line. Provide scene names.")]
+    private bool branch = false;
+    [SerializeField]
+    private string LineA, LineB;
+
+    private bool preloadStarted = false, loadComplete = false, lastScene = false;
+    public bool PreloadStarted { get { return preloadStarted; } }
+    public bool LoadComplete { get { return loadComplete; } }
+    public bool LastScene { get { return lastScene; } }
+
+    private AsyncOperation ao;
+    private int pageNumber;
+
+    void Awake()
+    {
+        pageNumber = GetComponent<PageManager>().PageNumber;
+    }
+
+    public void LoadNext(Choice playerChoice)
+    {
+        Page current;
+        //current.number = GameState.previousPage.number + 1;
+        current.number = pageNumber;
+        current.playerChoice = playerChoice;
+        GameState.previousPage = current;
+
+        // Don't push last scene
+        //if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCount)
+        GameState.PushBackPreviousPage();
+
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(Load());
+
+    }
+
+    public void StartPreloading()
+    {
+        if (!branch)
+            StartCoroutine(Preload());
+        else if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+            Debug.Log("Branch page detected, skipping preloading.");
+    }
+
+    IEnumerator Preload()
+    {
+        if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+            Debug.Log(name + pageNumber + " has started preloading.");
+
+        ao = new AsyncOperation();
+        int scene = SceneManager.GetActiveScene().buildIndex + 1;
+
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+
+        if (GameManager.Instance.DebugLevel >= DebugLevel.Verbose)
+            Debug.Log("next scene:" + scene + " scenecount:" + sceneCount);
+
+        if (scene < sceneCount)
+        {
+            ao = SceneManager.LoadSceneAsync(scene);
+            ao.allowSceneActivation = false;
+
+            // For Load()
+            preloadStarted = true;
+            loadComplete = false;
+
+            while (!loadComplete)
+            {
+                // [0, 0.9] > [0, 1]
+                float progress = Mathf.Clamp01(ao.progress / 0.9f);
+                float prev_frame_progress = -1;
+
+                if (GameManager.Instance.DebugLevel >= DebugLevel.Verbose && prev_frame_progress != progress)
+                    Debug.Log("Loading progress: " + (progress * 100) + "%");
+
+                prev_frame_progress = progress;
+
+                // Loading completed
+                if (ao.progress == 0.9f)
+                    loadComplete = true;
+
+                yield return null;
+            }
+
+            if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+                Debug.Log(name + pageNumber + " has finished preloading.");
+        }
+
+        else
+        {
+            lastScene = true;
+            if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+                Debug.Log("Preload Coroutine: This is the last scene.");
+        }
+    }
+
+    public IEnumerator Load()
+    {
+        if (!lastScene)
+        {
+            if (branch)
+            {
+                string lineToLoad = (GameState.previousPage.playerChoice == Choice.Left) ? LineA : LineB;
+                SceneManager.LoadScene(lineToLoad);
+            }
+
+            else if (preloadStarted)
+            {
+                while (!loadComplete)
+                    yield return null;
+                ao.allowSceneActivation = true;
+            }
+
+            else
+            {
+                if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+                    Debug.Log("Scene not pre-loaded, forcing.");
+
+                // Too messy to restart Load() after Preload() completes
+                //StartCoroutine(Preload());
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+
+        else
+        {
+            if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+                Debug.Log("No more scenes to load.");
+        }
+    }
+}
--- Scripts/PageManager.cs
+++ Scripts/PageManager.cs
@@ -8,13 +8,14 @@ public class PageManager : MonoBehaviour
     [Header("Scene")]
     [SerializeField]
     private int pageNumber;
+    public int PageNumber { get { return pageNumber; } }
     [SerializeField]
     private bool checkpoint = false;
-    [SerializeField] 
-    [Tooltip("Use this to branch to a different Line. Provide scene names.")]
-    private bool branch = false;
-    [SerializeField]
-    private string LineA, LineB;
+    //[SerializeField] 
+    //[Tooltip("Use this to branch to a different Line. Provide scene names.")]
+    //private bool branch = false;
+    //[SerializeField]
+    //private string LineA, LineB;
 
     
     [Header("Content")]
@@ -41,7 +42,7 @@ public class PageManager : MonoBehaviour
             Persistor.Save(SceneManager.GetActiveScene().name);
     }
 
-	protected virtual void Start ()
+	void Start ()
     {
         if (pageNumber == 0)
             Debug.Assert(false, "Check scene number");
@@ -139,102 +140,108 @@ public class PageManager : MonoBehaviour
         GameState.PushBackPreviousPage();
 
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        StartCoroutine(Load());
+        Loader loader = GetComponent<Loader>();
+        StartCoroutine(loader.Load());
         
     }
 
     public void StartPreloading()
     {
-        if (!branch)
-            StartCoroutine(Preload());
-        else if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
-            Debug.Log("Branch page detected, skipping preloading.");
+        GetComponent<Loader>().StartPreloading();
     }
+
+    //public void StartPreloading()
+    //{
+    //    if (!branch)
+    //        StartCoroutine(Preload());
+    //    else if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+    //        Debug.Log("Branch page detected, skipping preloading.");
+    //}
     
-    IEnumerator Preload()
-    {
-        if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
-            Debug.Log(name + pageNumber + " has started preloading.");
+    //IEnumerator Preload()
+    //{
+    //    if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+    //        Debug.Log(name + pageNumber + " has started preloading.");
 
-        ao = new AsyncOperation();
-        int scene = SceneManager.GetActiveScene().buildIndex + 1;
+    //    ao = new AsyncOperation();
+    //    int scene = SceneManager.GetActiveScene().buildIndex + 1;
 
-        int sceneCount = SceneManager.sceneCountInBuildSettings;
+    //    int sceneCount = SceneManager.sceneCountInBuildSettings;
 
-        if (GameManager.Instance.DebugLevel >= DebugLevel.Verbose)
-            Debug.Log("next scene:" + scene + " scenecount:" + sceneCount);
+    //    if (GameManager.Instance.DebugLevel >= DebugLevel.Verbose)
+    //        Debug.Log("next scene:" + scene + " scenecount:" + sceneCount);
 
-        if (scene < sceneCount)
-        {
-            ao = SceneManager.LoadSceneAsync(scene);
-            ao.allowSceneActivation = false;
+    //    if (scene < sceneCount)
+    //    {
+    //        ao = SceneManager.LoadSceneAsync(scene);
+    //        ao.allowSceneActivation = false;
 
-            // For Load()
-            preloadStarted = true;
-            loadComplete = false;
+    //        // For Load()
+    //        preloadStarted = true;
+    //        loadComplete = false;
 
-            while (!loadComplete)
-            {
-                // [0, 0.9] > [0, 1]
-                float progress = Mathf.Clamp01(ao.progress / 0.9f);
-                float prev_frame_progress = -1;
+    //        while (!loadComplete)
+    //        {
+    //            // [0, 0.9] > [0, 1]
+    //            float progress = Mathf.Clamp01(ao.progress / 0.9f);
+    //            float prev_frame_progress = -1;
 
-                if (GameManager.Instance.DebugLevel >= DebugLevel.Verbose && prev_frame_progress != progress)
-                    Debug.Log("Loading progress: " + (progress * 100) + "%");
+    //            if (GameManager.Instance.DebugLevel >= DebugLevel.Verbose && prev_frame_progress != progress)
+    //                Debug.Log("Loading progress: " + (progress * 100) + "%");
 
-                prev_frame_progress = progress;
+    //            prev_frame_progress = progress;
 
-                // Loading completed
-                if (ao.progress == 0.9f)
-                    loadComplete = true;
+    //            // Loading completed
+    //            if (ao.progress == 0.9f)
+    //                loadComplete = true;
 
-                yield return null;
-            }
+    //            yield return null;
+    //        }
 
-            if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
-                Debug.Log(name + pageNumber + " has finished preloading.");
-        }
+    //        if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+    //            Debug.Log(name + pageNumber + " has finished preloading.");
+    //    }
 
-        else
-        {
-            lastScene = true;
-            if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
-                Debug.Log("Preload Coroutine: This is the last scene.");
-        }
-    }
+    //    else
+    //    {
+    //        lastScene = true;
+    //        if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+    //            Debug.Log("Preload Coroutine: This is the last scene.");
+    //    }
+    //}
 
-    IEnumerator Load()
-    {
-        if (!lastScene)
-        {
-            if (branch)
-            {
-                string lineToLoad = (GameState.previousPage.playerChoice == Choice.Left) ? LineA : LineB;
-                SceneManager.LoadScene(lineToLoad);
-            }
+    //IEnumerator Load()
+    //{
+    //    if (!lastScene)
+    //    {
+    //        if (branch)
+    //        {
+    //            string lineToLoad = (GameState.previousPage.playerChoice == Choice.Left) ? LineA : LineB;
+    //            SceneManager.LoadScene(lineToLoad);
+    //        }
 
-            else if (preloadStarted)
-            {
-                while (!loadComplete)
-                    yield return null;
-                ao.allowSceneActivation = true;
-            }
+    //        else if (preloadStarted)
+    //        {
+    //            while (!loadComplete)
+    //                yield return null;
+    //            ao.allowSceneActivation = true;
+    //        }
 
-            else
-            {
-                if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
-                    Debug.Log("Scene not pre-loaded, forcing.");
+    //        else
+    //        {
+    //            if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+    //                Debug.Log("Scene not pre-loaded, forcing.");
 
-                // Too messy to restart Load() after Preload() completes
-                //StartCoroutine(Preload());
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            }
-        }
+    //            // Too messy to restart Load() after Preload() completes
+    //            //StartCoroutine(Preload());
+    //            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    //        }
+    //    }
 
-        else
-        {
-            if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
-                Debug.Log("No more scenes to load.");
-        }   
-    }
+    //    else
+    //    {
+    //        if (GameManager.Instance.DebugLevel >= DebugLevel.Notify)
+    //            Debug.Log("No more scenes to load.");
+    //    }   
+    //}
 }
